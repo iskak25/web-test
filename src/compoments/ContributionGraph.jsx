729ddcd6c22741412c00f9aff6509d219109fda7
iskak25 <97424184+iskak25@users.noreply.github.com>
2023-08-22 00:36:23 +0600

    i am sorry

--- src/compoments/ContributionGraph.jsx
+++ src/compoments/ContributionGraph.jsx
@@ -6,14 +6,18 @@ import { useEffect, useState } from 'react'
 const ContributionGraph = () => {
   const data = useSelector((state) => state.state?.data)
   const dispatch = useDispatch()
-  const [isHovered, setIsHovered] = useState(false)
+  const [hoveredBlock, setHoveredBlock] = useState(null)
 
-  const handleMouseEnter = () => {
-    setIsHovered(true)
+  function editDate(cout) {
+    return new Date(cout)
+  }
+
+  const handleMouseEnter = (blockId) => {
+    setHoveredBlock(blockId)
   }
 
   const handleMouseLeave = () => {
-    setIsHovered(false)
+    setHoveredBlock(null)
   }
   const skeleton = [...new Array(26)].map(() => (
     <li className={style.squares_empty}></li>
@@ -22,30 +26,95 @@ const ContributionGraph = () => {
     <li className={style.squares_empty}></li>
   ))
 
-  function sum(m, n) {
-    if (n == 0) {
-      console.log(n)
-      return <li className={style.squares_one} key={m} data-level={n}></li>
-    } else if (n < 9) {
-      console.log(n)
+  function sum(m, n, value) {
+    if (n < 9) {
       return (
-        <li
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          className={style.squares_two}
-          key={m}
-          data-level={n}
-        ></li>
+        <>
+          <li
+            onMouseEnter={() => handleMouseEnter(m)}
+            onMouseLeave={handleMouseLeave}
+            className={style.squares_one}
+            key={m}
+            data-level={n}
+          ></li>
+          {hoveredBlock == m ? (
+            <div className={style.container}>
+              <div className={style.deck}>
+                <p className={style.contributions}>{n} contributions </p>
+                <p className={style.dataId}>{value}</p>
+              </div>
+            </div>
+          ) : (
+            ''
+          )}
+        </>
       )
     } else if (n < 19) {
-      console.log(n)
-      return <li className={style.squares_three} key={m} data-level={n}></li>
+      return (
+        <>
+          <li
+            onMouseEnter={() => handleMouseEnter(m)}
+            onMouseLeave={handleMouseLeave}
+            className={style.squares_two}
+            key={m}
+            data-level={n}
+          ></li>
+          {hoveredBlock == m ? (
+            <div className={style.container}>
+              <div className={style.deck}>
+                <p className={style.contributions}>{n} contributions </p>
+                <p className={style.dataId}>{value}</p>
+              </div>
+            </div>
+          ) : (
+            ''
+          )}
+        </>
+      )
     } else if (n < 29) {
-      console.log(n)
-      return <li className={style.squares_four} key={m} data-level={n}></li>
+      return (
+        <>
+          <li
+            onMouseEnter={() => handleMouseEnter(m)}
+            onMouseLeave={handleMouseLeave}
+            className={style.squares_three}
+            key={m}
+            data-level={n}
+          ></li>
+          {hoveredBlock == m ? (
+            <div className={style.container}>
+              <div className={style.deck}>
+                <p className={style.contributions}>{n} contributions </p>
+                <p className={style.dataId}>{value}</p>
+              </div>
+            </div>
+          ) : (
+            ''
+          )}
+        </>
+      )
     } else if (n > 30) {
-      console.log(n)
-      return <li className={style.squares_five} key={m} data-level={n}></li>
+      return (
+        <>
+          <li
+            onMouseEnter={() => handleMouseEnter(m)}
+            onMouseLeave={handleMouseLeave}
+            className={style.squares_four}
+            key={m}
+            data-level={n}
+          ></li>
+          {hoveredBlock == m ? (
+            <div className={style.container}>
+              <div className={style.deck}>
+                <p className={style.contributions}>{n} contributions </p>
+                <p className={style.dataId}>{new Date(value)}</p>
+              </div>
+            </div>
+          ) : (
+            ''
+          )}
+        </>
+      )
     }
   }
 
@@ -86,20 +155,29 @@ const ContributionGraph = () => {
         </ul>
         <ul className={style.squares}>
           {skeleton}
-          {objectArray?.map((item) => (
-            <>{sum(item.key, item.value)}</>
+          {objectArray?.map((item, index) => (
+            <>{sum(index, item.value, item.key)}</>
           ))}
           {skeletons}
         </ul>
       </div>
-      {isHovered ? (
+      {/* {isHovered ? (
         <div className={style.deck}>
           <p className={style.contributions}>25 contributions </p>
           <p className={style.dataId}>Четверг, Ноябрь 28, 2022</p>
         </div>
       ) : (
         ''
-      )}
+      )} */}
+      <ul className={style.what}>
+        Использованные стек технологии
+        <li className={style.what}>React</li>
+        <li className={style.what}>Redux</li>
+        <li className={style.what}>Module SCSS</li>
+        <li className={style.what}>для запроса axios</li>
+        <li className={style.what}>буду рать любому отзыву</li>
+        <a href="https://t.me/iskak2512">Телеграм</a>
+      </ul>
     </>
   )
 }
